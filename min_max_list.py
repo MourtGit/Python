9e786fe353d7b532f7343dccf07fb8c54662f949
MourtGit <142163981+MourtGit@users.noreply.github.com>
2024-02-19 19:07:27 -0500

    Add files via upload

---
+++ min_max_list.py
@@ -0,0 +1,10 @@
+list=[0, 0, 0, 0]
+if list == []:
+        print([])
+else:
+   print(max(list), min(list))
+
+# list=max_min[3, 4, 5, 6] # Output: [6, 3]
+# list=[-1, -2, -3, -4] # Output: [-1, -4]
+# list=[0, 0, 0, 0] # Output: [0, 0]
+# list[] # Output: []
