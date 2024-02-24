bb98c69faf90e492bafe2f6e9cc6f5f0e93a91cd
MourtGit <142163981+MourtGit@users.noreply.github.com>
2024-02-24 11:29:27 -0500

    Add files via upload
    This program asks for two numbers (integer or float) and calculates the sum of those numbers

---
+++ sum-calculator.py
@@ -0,0 +1,6 @@
+# This program will ask for two numbers (integer or float) and calculates the sum of those two numbers.
+
+number1 = float(input("Enter the first number: "))
+number2=  float(input("Enter the second number: "))
+sum = number1+number2
+print (f"The sum of {number1} and {number2} is: {sum}")
