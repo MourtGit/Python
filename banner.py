1df3f7965fb8bd696d20bba0c2d691442beb6655
MourtGit <142163981+MourtGit@users.noreply.github.com>
2024-02-23 23:41:15 -0500

    Add files via upload
    Prints video game banner using escape characters and special characters like #.

---
+++ banner.py
@@ -0,0 +1,5 @@
+banner_text="# Welcome to Street Fighter 95: Darth Vader #"
+# name=input("Enter your name, fighter: ")
+length = len(banner_text)
+border = '#' * length
+print(border+"\n#"+" "*(length-2)+"#\n"+banner_text+"\n#"+" "*(length-2)+"#\n"+border)
