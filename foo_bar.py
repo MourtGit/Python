5ca8dc6e43b23366165301e08bdc0e3bdd73f6db
MourtGit <142163981+MourtGit@users.noreply.github.com>
2024-02-25 14:04:36 -0500

    Update foo_bar.py
    This program requests a number from 0-50 from a user:
    •for every multiple of 5 print "Foo",
    •for every multiple of 7 print "Bar"
    •and for every multiple of both 5 and 7 prints "FooBar" instead of the number.

--- foo_bar.py
+++ foo_bar.py
@@ -1,14 +1,14 @@
 # FooBar
 
 num = int(input("Enter a number between 0 and 50: "))
-if not 0 <= num <= 50:
-    print("Please enter a number between 0 and 50.")
-
-elif num % 5 == 0 and num % 7 == 0:
-        print ("FooBar")
-elif num % 5 == 0:
-        print("Foo")
-elif num % 7 == 0:
-        print("Bar")
+if 0 <= num <= 50:
+        if num % 5 == 0 and num % 7 == 0:
+                print ("FooBar")
+        elif num % 5 == 0:
+                print("Foo")
+        elif num % 7 == 0:
+                print("Bar")
+        else:
+                print (num)
 else:
-    print (num)
+        print("Invalid! Please enter a number between 0 and 50.")
